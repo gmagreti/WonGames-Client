220d13951ece4d45946fb9591de853318f0dbba6
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-09-04 15:27:40 -0300

    Populando os mocks na page Wishlist

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,5 +1,18 @@
-import Whishlist from 'templates/Whishlist'
+import Whishlist, { WishlistTemplateProps } from 'templates/Whishlist'
 
-export default function WishlistPage() {
-  return <Whishlist />
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+export default function WishlistPage({ ...props }: WishlistTemplateProps) {
+  return <Whishlist {...props} />
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      games: gamesMock,
+      recommendedGames: gamesMock.slice(0, 4),
+      recommendedHighlight: highlightMock
+    }
+  }
 }
--- src/templates/Whishlist/index.tsx
+++ src/templates/Whishlist/index.tsx
@@ -1,17 +1,41 @@
 import { Container } from 'components/Container'
+import GameCard, { GameCardProps } from 'components/GameCard'
 import Heading from 'components/Heading'
+import { HighlightProps } from 'components/Highlight'
+import Showcase from 'components/Showcase'
 import Base from 'templates/Base'
 
 import * as S from './styles'
 
-const Whishlist = () => (
-  <Base>
-    <Container>
-      <Heading lineLeft lineColor="secondary" color="white">
-        Wishlist
-      </Heading>
-    </Container>
-  </Base>
-)
+export type WishlistTemplateProps = {
+  games?: GameCardProps[]
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
 
+const Whishlist = ({
+  games,
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => {
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary" color="white">
+          Wishlist
+        </Heading>
+
+        {games?.map((game, index) => (
+          <GameCard key={`wishlist-${index}`} {...game} />
+        ))}
+      </Container>
+
+      <Showcase
+        title="You may like these games"
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  )
+}
 export default Whishlist
