e7051b55263658930b56790358e241c04725a3ad
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-09-04 13:25:39 -0300

    Fix bug link in home

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -27,11 +27,18 @@ const Menu = ({ username }: MenuProps) => {
       </MediaMatch>
 
       <S.LogoWrapper>
-        <Logo hideOnMobile />
+        <Link href="/" passHref>
+          <a>
+            <Logo hideOnMobile />
+          </a>
+        </Link>
       </S.LogoWrapper>
 
       <MediaMatch greaterThan="medium">
-        <S.MenuLink href="#">Home</S.MenuLink>
+        <Link href="/" passHref>
+          <S.MenuLink href="#">Home</S.MenuLink>
+        </Link>
+
         <S.MenuLink href="#">Explore</S.MenuLink>
       </MediaMatch>
 
@@ -56,7 +63,9 @@ const Menu = ({ username }: MenuProps) => {
       <S.MenuFull isOpen={isOpen} aria-hidden={!isOpen}>
         <CloseIcon aria-label="Close menu" onClick={() => setIsOpen(false)} />
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink href="#">Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Explore</S.MenuLink>
 
           {!!username && (
