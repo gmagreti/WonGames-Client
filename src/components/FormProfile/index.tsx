88628894b0f0df1b2156afa7b925b6439d687612
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-09-08 00:34:05 -0300

    Finalizando component FormProfile

--- src/components/FormProfile/index.tsx
+++ src/components/FormProfile/index.tsx
@@ -1,8 +1,47 @@
+import TextField from 'components/TextField'
+import Heading from 'components/Heading'
+import Button from 'components/Button'
 import * as S from './styles'
 
 const FormProfile = () => (
   <S.Wrapper>
-    <h1>FormProfile</h1>
+    <Heading lineBottom color="black" size="small">
+      My profile
+    </Heading>
+
+    <S.Form>
+      <TextField
+        name="name"
+        placeholder="Name"
+        label="Name"
+        initialValue="Gabriel"
+      />
+
+      <TextField
+        name="email"
+        type="email"
+        placeholder="E-mail"
+        label="E-mail"
+        initialValue="gabriel@gabriel.com"
+        disabled
+      />
+
+      <TextField
+        name="password"
+        type="password"
+        placeholder="Type your password"
+        label="Password"
+      />
+
+      <TextField
+        name="new_password"
+        type="password"
+        placeholder="New password"
+        label="New password"
+      />
+
+      <Button size="large">Save</Button>
+    </S.Form>
   </S.Wrapper>
 )
 
