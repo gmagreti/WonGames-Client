958f359c1459e074d81249af9b9138610e1f3b7a
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-09-10 08:54:14 -0300

    Refatorando menu

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,13 +1,13 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
-export const Wrapper = styled.menu`
-  ${({ theme }) => css`
+export const Wrapper = styled.menu<MenuFullProps>`
+  ${({ theme, isOpen }) => css`
     display: flex;
     align-items: center;
     padding: ${theme.spacings.small} 0;
     position: relative;
-    z-index: ${theme.layers.menu};
+    z-index: ${isOpen ? theme.layers.menu : `calc(${theme.layers.menu} - 1)`};
   `}
 `
 export const IconWrapper = styled.div`
