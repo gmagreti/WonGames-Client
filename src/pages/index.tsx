73f421f4f0e3912eaea09587316a26f398ecca12
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-10-15 14:10:37 -0300

    populating banners on the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,34 @@ import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
+import { initializeApollo } from 'utils/apollo'
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
+
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 10,
+      banners: data.banners.map((banner) => ({
+        img: banner.image?.url,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label,
+        buttonLink: banner.button?.link,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon.text,
+          ribbonColor: banner.ribbon.color,
+          ribbonSize: banner.ribbon.size
+        })
+      })),
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
