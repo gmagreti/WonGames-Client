a8595faf2cb02fe37dc097e5cd031d32e08a2d71
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-02-02 12:04:09 -0300

    Feat: adjusting cart list with new context

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -6,7 +6,9 @@ describe('<CartList />', () => {
   it('should render the heading', () => {
     const { container } = render(<CartList />)
 
-    expect(screen.getByRole('heading', { name: /CartList/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /CartList/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
