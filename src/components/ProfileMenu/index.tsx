c7c1bade71a3ba69039195dfeaf01a40ecb67a69
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-07-13 22:44:14 -0300

    feat: adding function signout.

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { signOut } from 'next-auth/client'
 
 import { CreditCard } from '@styled-icons/material-outlined'
 import {
@@ -36,12 +37,16 @@ const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
       </S.Link>
     </Link>
 
-    <Link href="/logout" passHref>
-      <S.Link>
-        <ExitToApp size={24} />
-        <span>Sign out</span>
-      </S.Link>
-    </Link>
+    <S.Link
+      role="button"
+      onClick={(e) => {
+        e.preventDefault()
+        signOut()
+      }}
+    >
+      <ExitToApp size={24} />
+      <span>Sign out</span>
+    </S.Link>
   </S.Nav>
 )
 
